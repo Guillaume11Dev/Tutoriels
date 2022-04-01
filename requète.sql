5b73ee09b98213177e415665219ab964f7ea3fc3
guillaume dumoulin <97092035+Guillaume11Dev@users.noreply.github.com>
2022-04-01 14:09:49 +0200

    Update requète.sql

--- requète.sql
+++ requète.sql
@@ -1,5 +1,8 @@
+commande pour créé la vue :
+
 Create view PhotoMoyennes as( Select refPhoto , avg(note) as noteMoyenne from Noter group by refPhoto;
 
-
+requète : 
+                             
 Select refPhoto, noteMoyenne from PhotoMoyennes where noteMoyenne = (Select max(noteMoyenne) from PhotoMoyennes);
 
